cf7a736354d8f340ff276dafe2d075d9bf887d8c
윤석 한 <hys6078@naver.com>
2021-01-22 16:35:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gray-background">
+        <img src={logo} lat="logo"/>
+        <h2>let's develop management system!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
