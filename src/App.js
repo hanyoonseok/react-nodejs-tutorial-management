c9664ce9dcab737fda001b46e8355ad86ff70534
윤석 한 <hys6078@naver.com>
2021-01-22 17:26:05 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customers=[
+{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'yoonseok',
+  'birthday':'970130',
+  'gender':'male',
+  'job':'student'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'someone1',
+  'birthday':'111111',
+  'gender':'male',
+  'job':'student'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'asdfds3',
+  'birthday':'222222',
+  'gender':'female',
+  'job':'student'
+}
+]
 
 function App() {
   return (
-    <div className="App">
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>let's develop management system!</h2>
-      </div>
-    </div>
-  );
+    <div>
+      {
+        customers.map(c=>{
+          return (
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />);
+        })
+      }
+    </div>  
+    );
 }
 
 export default App;
